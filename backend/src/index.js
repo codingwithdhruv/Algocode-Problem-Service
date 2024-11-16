8a10db3ad8b163378f280fdb67257b9a29c1abbb
codingwithdhruv <techdhruvspace@gmail.com>
2024-11-17 00:18:24 +0530

    Feature: Added routing struct for problems

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { PORT } = require('../server.config');
+const { PORT } = require('../src/config/server.config');
+const apiRouter = require('../src/routes')
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
+
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
     return res.json({message: 'Problem service is alive'});
@@ -14,7 +18,3 @@ app.get('/ping', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server started at PORT: ${PORT}`)
 });
-
-
-
-
