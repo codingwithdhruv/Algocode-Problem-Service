0fe1666dd536941ff670f10ffacf91b608b32b89
codingwithdhruv <techdhruvspace@gmail.com>
2024-11-23 19:18:06 +0530

    added mongoose db config

--- backend/src/config/server.config.js
+++ backend/src/config/server.config.js
@@ -4,4 +4,5 @@ dotenv.config(); // loads env variables from .env file
 
 module.exports = {
     PORT: process.env.PORT || 3000,
+    ATLAS_DB_URL: process.env.ATLAS_DB_URL
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('../src/config/server.config');
 const apiRouter = require('../src/routes');
 const errorHandler = require('./utils/errorHandler');
+const connectToDb = require('./config/db.config');
 
 const app = express();
 
@@ -19,6 +20,8 @@ app.get('/ping', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server started at PORT: ${PORT}`)
-    });
+    await connectToDb();
+    console.log("Successfully connected to db");
+})
