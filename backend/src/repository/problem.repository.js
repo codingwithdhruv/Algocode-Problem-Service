75d5c92d4db7ccd9e0214ce9ffd79c0a1a9f4a2c
codingwithdhruv <techdhruvspace@gmail.com>
2024-11-25 23:10:12 +0530

    Feat: added get all problems api

--- backend/src/repository/problem.repository.js
+++ backend/src/repository/problem.repository.js
@@ -18,9 +18,11 @@ class ProblemRepository {
 
     async getAllProblems() {
         try {
-            const problems = await Problem.find({})
+            const problems = await Problem.find({});
+            return problems;
         } catch (error) {
-            
+            console.log(error);
+            throw error;
         }
     }
 
